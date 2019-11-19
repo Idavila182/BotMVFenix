82c76707558361ed7df5cc198abb3790e7290dd2
Ignacio <idavila182@gmail.com>
2019-11-19 22:45:15 +0100

    Correción mención

--- mencion.js
+++ mencion.js
@@ -4,16 +4,24 @@ var App = function () { };
 var st1 = "@Escipion87  @El_Comediante @Haokarlos @antpgar"/*@Xterminator81 @Goldo4125 Juan @Quinta90 */
 var st2 = " @CarreMSF @Orchryst @Lapipol" /* Luis @JCH_0 @AITORDELIA @Mononoke0614 @Kurgan */
 var st3 = "@IDavila @Ismaelaz  @Puigdemont" /* Tomqui @AleixRisco @saetix @Lia_lii @DiGuti85*/
-const listaCapis = ["Carre", "El_Comediante", "Haokarlos", "David", "Ismaelaz", "IDavila", "antpgar", "Escipion87", "Puigdemont"];
+const listaCapis = ["CarreMSF", "El_Comediante", "Haokarlos", "Orchryst", "Ismaelaz", "IDavila", "antpgar", "Escipion87", "Puigdemont"];
 const andaluces = ["antpgar", "Escipion87", "Puigdemont"];
 App.mencion = function (msg, bot) {
     var nameUser = msg.from.username;
     if (nameUser == undefined) {
         nameUser = msg.from.first_name;
     }
-    if (listaCapis.includes(nameUser)) {
+    if (listaCapis.includes(nameUser=="")) {
         if (andaluces.includes(nameUser)) {
+            bot.sendMessage(msg.chat.id,"No intentes engañarnos @"+msg.from.username+". Sabemos que sigues de sieta.").then(function (data) {
+                bot.sendPhoto(data.chat.id, "./imagenes/siesta.jpg")
+            });
 
+        }
+        else if(nameUser=="Ismaelaz"){
+            bot.sendMessage(msg.chat.id,"Por favor @"+msg.from.username+" este comando es muy importante. Confirma que estas en condiciones.").then(function (data) {
+                bot.sendPhoto(data.chat.id, "./imagenes/siesta.jpg")
+            });
         }
         else {
             bot.deleteMessage(msg.chat.id, msg.message_id);
